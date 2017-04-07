22701798bea6d9c638f76b6fbf22dc674c056d41
Taysiman Haque <thaque@umass.edu>
2017-04-07 15:45:06 -0400

    fb7

--- app/app.js
+++ app/app.js
@@ -151,6 +151,43 @@ class Feed extends React.Component {
   }
 }
 
+/**
+ * A fake profile page.
+ */
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <p>This is the profile page for a user with ID {this.props.params.id}.</p>
+    );
+  }
+}
+
+/**
+ * The Feed page. We created a new component just to fix the userId at 4.
+ */
+class FeedPage extends React.Component {
+  render() {
+    return <Feed user={4} />;
+  }
+}
+
+
+/**
+ * The primary component in our application.
+ * The Router will give it different child Components as the user clicks
+ * around the application.
+ *
+ * If we implemented all of Facebook, this App would also contain Component
+ * objects for the left and right content panes.
+ */
+class App extends React.Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
