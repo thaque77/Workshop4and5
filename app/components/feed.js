f3939a7441ae95db8ecccdeda374b4f90af1b3d1
Taysiman Haque <thaque@umass.edu>
2017-04-07 15:37:33 -0400

    fb6

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,13 +1,45 @@
+import {getFeedData} from '../server';
 import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
 
 export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: []
+    };
+  }
+
+  refresh() {
+    getFeedData(this.props.user, (feedData) => {
+      this.setState(feedData);
+    });
+  }
+
+  onPost(postContents) {
+    // Send to server.
+    // We could use geolocation to get a location, but let's fix it to Amherst
+    // for now.
+    postStatusUpdate(4, "Amherst, MA", postContents, () => {
+      // Database is now updated. Refresh the feed.
+      this.refresh();
+    });
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
   render() {
     return (
       <div>
-        <StatusUpdateEntry />
-        <FeedItem />
+        <StatusUpdateEntry onPost={(postContents) => this.onPost(postContents)} />
+        {this.state.contents.map((feedItem) => {
+          return (
+            <FeedItem key={feedItem._id} data={feedItem} />
+          )
+        })}
       </div>
     )
   }
