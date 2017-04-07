6128b43aa4b11d4b476c87a1d2c30fc572927c43
Taysiman Haque <thaque@umass.edu>
2017-04-07 15:37:43 -0400

    fb6

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Feed from './components/feed';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router'
 
 class Feed extends React.Component {
   render() {
@@ -150,7 +151,12 @@ class Feed extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <Feed />,
-  document.getElementById('fb-feed')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Show the Feed at / */}
+      <IndexRoute component={FeedPage} />
+      <Route path="profile/:id" component={ProfilePage} />
+    </Route>
+  </Router>
+),document.getElementById('fb-feed'));
